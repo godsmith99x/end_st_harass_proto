af8cd7265c877acab196b0cbc9efd52e8564c3be
godsmith <jgodfrey-smith@pm.me>
2022-02-23 15:12:15 -0500

    Set video constraints to be 90% of window inner height and width

--- webcam.js
+++ webcam.js
@@ -6,28 +6,24 @@ const snap = document.getElementById("snap");
 const errMsgElement = document.getElementById("span#ErrorMsg");
 
 const constraints = {
-	audio: true,
-	video: {
-		width: 1280,
-		height: 720,
-	},
+  audio: true,
+  video: {
+    width: window.innerWidth * 0.9,
+    height: window.innerHeight * 0.9,
+  },
 };
 
 // Access webcam
 async function init() {
-	try {
-		const stream = await navigator.mediaDevices.getUserMedia(
-			constraints
-		);
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
 
-		window.stream = stream;
-		video.srcObject = stream;
-
-	} catch (err) {
-		errMsgElement.innerHTML = `navigator.getUserMedia.error: ${err.toString()}`;
-	}
+    window.stream = stream;
+    video.srcObject = stream;
+  } catch (err) {
+    errMsgElement.innerHTML = `navigator.getUserMedia.error: ${err.toString()}`;
+  }
 }
-
 
 // Call Init function to start webcam
 init();
@@ -35,5 +31,5 @@ init();
 // Draw image
 let context = canvas.getContext("2d");
 snap.addEventListener("click", function () {
-	context.drawImage(video, 0, 0, 648, 480);
+  context.drawImage(video, 0, 0, 648, 480);
 });
